933abdabddec15ee94bc84ab4a3369e02422c136
Fadila Fidina <fadila_fidina@hotmail.com>
2020-12-06 06:32:14 +1100

    cleaned up and working

--- src/App.js
+++ src/App.js
@@ -4,11 +4,6 @@ export class App extends Component {
     constructor() {
         super();
         this.state = {
-            // firstName: '',
-            // lastName: '',
-            // age: '',
-            // gender: '',
-            // location: '',
             totalFlour: '',
             breadFlour: '',
             wwFlour: '',
@@ -31,6 +26,7 @@ export class App extends Component {
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.getDietStrings = this.getDietStrings.bind(this);
+        this.getTotalMass = this.getTotalMass.bind(this);
     }
 
     handleChange(event) {
@@ -38,38 +34,18 @@ export class App extends Component {
         const { name, type, value, checked } = event.target;
 
         console.log(`name: ${name}, value: ${value}`)
-
-        // this.setState({
-        //     [name]: value
-        // });
-
-        // type === 'checkbox'
-        //     ? this.setState({
-        //         dietaryRequirements: {
-        //             [name]: checked
-        //         }
-        //     })
-        //     : this.setState({
-        //         [name]: value
-        //     });
-
         name === 'totalFlour'
             ? this.setState({
-                // [name]: value,
-                // [name]: this.state.totalFlour * this.state.ratios[name],
                 totalFlour: value,
-                breadFlour: value * 0.8,
-                wwFlour: value * 0.2,
-                levain: value * 0.2,
-                water: value * 0.7,
-                salt: value * 0.02,
+                breadFlour: value / this.state.ratios[name] * this.state.ratios.breadFlour,
+                wwFlour: value / this.state.ratios[name] * this.state.ratios.wwFlour,
+                levain: value / this.state.ratios[name] * this.state.ratios.levain,
+                water: value / this.state.ratios[name] * this.state.ratios.water,
+                salt: value / this.state.ratios[name] * this.state.ratios.salt,
             })
             : name === 'breadFlour'
                 ? this.setState({
-                    // [name]: value,
-                    // [name]: this.state.totalFlour * this.state.ratios[name],
-                    // let perUnit = value / this.state.ratios.breadFlour;
-                    totalFlour: value / this.state.ratios.breadFlour * 1,
+                    totalFlour: value / this.state.ratios.breadFlour * this.state.ratios.totalFlour,
                     breadFlour: value,
                     wwFlour: value / this.state.ratios.breadFlour * this.state.ratios.wwFlour,
                     levain: value / this.state.ratios.breadFlour * this.state.ratios.levain,
@@ -87,12 +63,8 @@ export class App extends Component {
                         levain: value * 0.2,
                         water: value * 0.7,
                         salt: value * 0.02,
-                    }) :
-
-
-
-
-                    console.log(this.state);
+                    }) : console.log(this.state);
+        console.log(this.state);
     };
 
     handleSubmit(event) {
@@ -123,28 +95,22 @@ export class App extends Component {
 
     }
 
-
+    getTotalMass() {
+        let total = +this.state.totalFlour +
+            +this.state.levain +
+            +this.state.salt +
+            +this.state.water;
+        return total || 0;
+    }
 
     render() {
-
         return (
             <div>
-                {/* <form>
-                    <input
-                        name='Total flour' // name was wrong
-                        value={this.state.totalFlour}
-                        type='text'
-                        onChange={this.handleChange}>
-                    </input>
-                </form> */}
-
-
-
                 <form>
                     Total flour:
                     <input
                         name='totalFlour'
-                        value={this.state.totalFlour}
+                        value={this.state.totalFlour === 0 ? '' : this.state.totalFlour}
                         type='text'
                         placeholder='Total Flour'
                         onChange={this.handleChange}>
@@ -153,7 +119,7 @@ export class App extends Component {
                     Bread flour:
                     <input
                         name='breadFlour'
-                        value={this.state.breadFlour}
+                        value={this.state.breadFlour === 0 ? '' : this.state.breadFlour}
                         type='text'
                         placeholder='Bread Flour'
                         onChange={this.handleChange}>
@@ -162,7 +128,7 @@ export class App extends Component {
                     WW flour:
                     <input
                         name='wwFlour'
-                        value={this.state.wwFlour}
+                        value={this.state.wwFlour === 0 ? '' : this.state.wwFlour}
                         type='text'
                         placeholder='Whole wheat flour'
                         onChange={this.handleChange}>
@@ -171,7 +137,7 @@ export class App extends Component {
                     Levain:
                     <input
                         name='levain'
-                        value={this.state.levain}
+                        value={this.state.levain === 0 ? '' : this.state.levain}
                         type='text'
                         placeholder='Levain'
                         onChange={this.handleChange}>
@@ -180,159 +146,18 @@ export class App extends Component {
                     Salt:
                     <input
                         name='salt'
-                        value={this.state.salt}
+                        value={this.state.salt === 0 ? '' : this.state.salt}
                         type='text'
                         placeholder='Salt'
                         onChange={this.handleChange}>
                     </input>
                     <br />
 
-                    <input
-                        name='firstName'
-                        value={this.state.firstName}
-                        type='text'
-                        placeholder='First Name'
-                        onChange={this.handleChange}>
-                    </input>
+                    Total mass: {this.getTotalMass()}
                     <br />
-                    <input
-                        name='lastName'
-                        value={this.state.lastName}
-                        type='text'
-                        placeholder='Last Name'
-                        onChange={this.handleChange}>
-                    </input>
-                    <br />
-
-                    <input
-                        name='age'
-                        value={this.state.age}
-                        type='number'
-                        placeholder='Age'
-                        onChange={this.handleChange}>
-                    </input>
-                    <br />
-
-                    <label>
-                        <input
-                            name='gender'
-                            type='radio'
-                            value='female'
-                            // checked={this.state.value === 'female'}
-                            onChange={this.handleChange}>
-                        </input>
-                        Female
-                    </label>
-                    <br />
-
-                    <label>
-                        <input
-                            name='gender'
-                            type='radio'
-                            value='male'
-                            // checked={this.state.value === 'male'}
-                            onChange={this.handleChange}>
-                        </input>
-                        Male
-                    </label>
-
-                    <br />
-
-
-                    {/* // value is what the value of the state should be if it is chosen */}
-                    <select
-                        name='location'
-                        onChange={this.handleChange}>
-                        <option value=''>Please choose an option</option>
-                        <option value='kyoto'>Kyoto</option>
-                        <option value='osaka'>Osaka</option>
-                        <option value='london'>London</option>
-                        <option value='new york'>New York</option>
-                    </select>
-
-                    <br />
-                    <h2>Dietary requirements</h2>
-
-                    <br />
-                    <label>
-                        <input
-                            name='isHalal'
-                            type='checkbox'
-                            checked={this.state.dietaryRequirements.isHalal}
-                            onChange={this.handleChange}>
-                        </input>
-                        Halal
-                    </label>
-
-                    <br />
-
-                    <label>
-                        <input
-                            name='isVegetarian'
-                            type='checkbox'
-                            checked={this.state.dietaryRequirements.isVegetarian}
-                            onChange={this.handleChange}>
-                        </input>
-                        Vegetarian
-                    </label>
-
-
-
-
-
-                    <button onClick={this.handleSubmit}>Submit</button>
                 </form>
 
                 <hr></hr>
-
-                <h1>
-                    Your details are displayed below.
-                </h1>
-
-
-                <p>
-                    Total flour: {this.state.totalFlour}
-                </p>
-
-                <p>
-                    Bread flour: {this.state.breadFlour}
-                </p>
-
-                <p>
-                    WW flour: {this.state.totalFlour * 0.30}
-                </p>
-
-                <p>
-                    Water: {this.state.totalFlour * 0.7}
-                </p>
-
-                <p>
-                    Levain: {this.state.totalFlour * 0.2}
-                </p>
-
-                <p>
-                    Salt: {this.state.salt * 0.02}
-                </p>
-
-                {/*                 
-                <p>
-                    Last name: {this.state.lastName}
-                </p>
-                <p>
-                    Age: {this.state.age}
-                </p>
-
-                <p>
-                    Gender: {this.state.gender}
-                </p>
-
-                <p>
-                    Location: {this.state.location}
-                </p>
-
-                <p>
-                    Dietary Requirements: {this.getDietStrings()}
-                </p> */}
             </div>
         )
     }
