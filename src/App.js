ebbc3ccd7fa4a71348ae5ab90732e40e4e17a46b
Fadila Fidina <fadila_fidina@hotmail.com>
2020-12-06 05:58:54 +1100

    some dodgy calculator going

--- src/App.js
+++ src/App.js
@@ -4,11 +4,25 @@ export class App extends Component {
     constructor() {
         super();
         this.state = {
-            firstName: '',
-            lastName: '',
-            age: '',
-            gender: '',
-            location: '',
+            // firstName: '',
+            // lastName: '',
+            // age: '',
+            // gender: '',
+            // location: '',
+            totalFlour: '',
+            breadFlour: '',
+            wwFlour: '',
+            levain: '',
+            water: '',
+            salt: '',
+            ratios: {
+                totalFlour: 1,
+                breadFlour: 0.8,
+                wwFlour: 0.2,
+                levain: 0.2,
+                water: 0.7,
+                salt: 0.02,
+            },
             dietaryRequirements: {
                 isHalal: false,
                 isVegetarian: false,
@@ -29,17 +43,56 @@ export class App extends Component {
         //     [name]: value
         // });
 
-        type === 'checkbox'
-            ? this.setState({
-                dietaryRequirements: {
-                    [name]: checked
-                }
-            })
-            : this.setState({
-                [name]: value
-            });
+        // type === 'checkbox'
+        //     ? this.setState({
+        //         dietaryRequirements: {
+        //             [name]: checked
+        //         }
+        //     })
+        //     : this.setState({
+        //         [name]: value
+        //     });
 
-        console.log(this.state);
+        name === 'totalFlour'
+            ? this.setState({
+                // [name]: value,
+                // [name]: this.state.totalFlour * this.state.ratios[name],
+                totalFlour: value,
+                breadFlour: value * 0.8,
+                wwFlour: value * 0.2,
+                levain: value * 0.2,
+                water: value * 0.7,
+                salt: value * 0.02,
+            })
+            : name === 'breadFlour'
+                ? this.setState({
+                    // [name]: value,
+                    // [name]: this.state.totalFlour * this.state.ratios[name],
+                    // let perUnit = value / this.state.ratios.breadFlour;
+                    totalFlour: value / this.state.ratios.breadFlour * 1,
+                    breadFlour: value,
+                    wwFlour: value / this.state.ratios.breadFlour * this.state.ratios.wwFlour,
+                    levain: value / this.state.ratios.breadFlour * this.state.ratios.levain,
+                    water: value / this.state.ratios.breadFlour * this.state.ratios.water,
+                    salt: value / this.state.ratios.breadFlour * this.state.ratios.salt,
+
+                })
+                : name === 'wwFlour'
+                    ? this.setState({
+                        // [name]: value,
+                        // [name]: this.state.totalFlour * this.state.ratios[name],
+                        totalFlour: value,
+                        breadFlour: value * 0.8,
+                        wwFlour: value * 0.2,
+                        levain: value * 0.2,
+                        water: value * 0.7,
+                        salt: value * 0.02,
+                    }) :
+
+
+
+
+                    console.log(this.state);
     };
 
     handleSubmit(event) {
@@ -76,7 +129,64 @@ export class App extends Component {
 
         return (
             <div>
+                {/* <form>
+                    <input
+                        name='Total flour' // name was wrong
+                        value={this.state.totalFlour}
+                        type='text'
+                        onChange={this.handleChange}>
+                    </input>
+                </form> */}
+
+
+
                 <form>
+                    Total flour:
+                    <input
+                        name='totalFlour'
+                        value={this.state.totalFlour}
+                        type='text'
+                        placeholder='Total Flour'
+                        onChange={this.handleChange}>
+                    </input>
+                    <br />
+                    Bread flour:
+                    <input
+                        name='breadFlour'
+                        value={this.state.breadFlour}
+                        type='text'
+                        placeholder='Bread Flour'
+                        onChange={this.handleChange}>
+                    </input>
+                    <br />
+                    WW flour:
+                    <input
+                        name='wwFlour'
+                        value={this.state.wwFlour}
+                        type='text'
+                        placeholder='Whole wheat flour'
+                        onChange={this.handleChange}>
+                    </input>
+                    <br />
+                    Levain:
+                    <input
+                        name='levain'
+                        value={this.state.levain}
+                        type='text'
+                        placeholder='Levain'
+                        onChange={this.handleChange}>
+                    </input>
+                    <br />
+                    Salt:
+                    <input
+                        name='salt'
+                        value={this.state.salt}
+                        type='text'
+                        placeholder='Salt'
+                        onChange={this.handleChange}>
+                    </input>
+                    <br />
+
                     <input
                         name='firstName'
                         value={this.state.firstName}
@@ -178,9 +288,33 @@ export class App extends Component {
                 <h1>
                     Your details are displayed below.
                 </h1>
+
+
                 <p>
-                    First name: {this.state.firstName}
+                    Total flour: {this.state.totalFlour}
                 </p>
+
+                <p>
+                    Bread flour: {this.state.breadFlour}
+                </p>
+
+                <p>
+                    WW flour: {this.state.totalFlour * 0.30}
+                </p>
+
+                <p>
+                    Water: {this.state.totalFlour * 0.7}
+                </p>
+
+                <p>
+                    Levain: {this.state.totalFlour * 0.2}
+                </p>
+
+                <p>
+                    Salt: {this.state.salt * 0.02}
+                </p>
+
+                {/*                 
                 <p>
                     Last name: {this.state.lastName}
                 </p>
@@ -198,7 +332,7 @@ export class App extends Component {
 
                 <p>
                     Dietary Requirements: {this.getDietStrings()}
-                </p>
+                </p> */}
             </div>
         )
     }
