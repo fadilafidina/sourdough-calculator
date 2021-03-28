19ed553dc01c7f3119b05072d2c923dac9b8fe13
Fadila Fidina <fadila_fidina@hotmail.com>
2021-03-28 11:39:11 +1100

    refactor ingredient component into function component

--- src/Ingredient.js
+++ src/Ingredient.js
@@ -1,46 +1,33 @@
 import React from 'react';
 
-export default class Ingredient extends React.Component {
-    constructor(props) {
-        super(props);
-        console.log("props from child!!!!!!!!!!!!!", props);
-    }
+export default function Ingredient(props) {
+    const ratioToDisplay = props.ratio === 0 ? '' : props.ratio * 100;
 
-    render() {
-        let ratioToDisplay = this.props.ratio === 0 ? '' : this.props.ratio * 100;
-
-        return (
-            <div class="pair">
-                <div class="left">
-
-                    <div class="label">
-                        {this.props.label}
-                    </div>
-
-                    <input
-                        name={this.props.text}
-                        value={ratioToDisplay}
-                        type='text'
-                        placeholder={`Ratio of ${this.props.label}`} // no place holder
-                        onChange={(e) => this.props.handleChangeRatio(e, this.props.id)}>
-                    </input> %
-                    <br />
+    return (
+        <div class="pair">
+            <div class="left">
+                <div class="label">
+                    {props.label}
+                </div>
+                <input
+                    name={props.text}
+                    value={ratioToDisplay}
+                    type='text'
+                    placeholder={`Ratio of ${props.label}`} // no place holder
+                    onChange={(e) => props.handleChangeRatio(e, props.id)}>
+                </input> %
                 </div >
 
-                < div class="right">
-
-                    Amount:
-                <input
-                        name={this.props.text}
-                        value={this.props.amount === 0 ? '' : this.props.amount}
-                        type='text'
-                        placeholder={this.props.label}
-                        onChange={(e) => this.props.handleChange(e, this.props.id)}>
-                    </input> grams
-
-                < br />
+            <div class="right">
+                Amount:
+                    <input
+                    name={props.text}
+                    value={props.amount === 0 ? '' : props.amount}
+                    type='text'
+                    placeholder={props.label}
+                    onChange={(e) => props.handleChange(e, props.id)}>
+                </input> grams
                 </div>
-            </div >
-        )
-    }
+        </div >
+    )
 }
